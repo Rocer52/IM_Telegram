2c13cca64033f58badf636c39ed15e052b7a91f7
Rocer52 <batte9306@gmail.com>
2024-12-21 12:16:08 +0800

    v14

--- version_1/config.py
+++
@@ -1,9 +0,0 @@
-# config.py 
-# **please set up Environment variable in your deployment**
-import os
-
-# If it's not set, you can set a default value or handle the error
-TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN', 'default')
-
-print(f"Using Access Token: {TELEGRAM_BOT_TOKEN}")
-# You can add additional configuration settings here if needed
